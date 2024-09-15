d70074170af479bd85ec3653eaa1f6bda9f1266b
Pablo Gómez-Caldito <pablo@caldito.me>
2024-09-15 15:53:37 +0200

    First working version tail

--- tail.py
+++ tail.py
@@ -1,10 +1,31 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3 
 
 # Implement a tail(1)-like tool, which prints last N lines for a file provided as a param and watch for the new entries and print them on stdout until interrupted (tail -f -n N equivalent).
 
+import argparse
 import sys
 
-def main(path,follow):
+
+
+def parse_args():
+    print("hello")
+    parser = argparse.ArgumentParser(description='Optional app description')
+
+    parser.add_argument('path',
+                    help='follow new entries')
+    parser.add_argument('-n',
+                    required = False,
+                    type=int,
+                    help='follow new entries')
+    parser.add_argument('-f', '--follow',
+                    required = False,
+                    action='store_true',
+                    help='follow new entries')
+
+    args = parser.parse_args()
+    return args.path, args.follow, args.n
+
+def tail(path, follow, lines):
     fd = open(path)
     file_splitted_by_lines = fd.readlines() # array of lines
     number_lines = len(file_splitted_by_lines)
@@ -12,7 +33,7 @@ def main(path,follow):
     last_lines = file_splitted_by_lines[number_lines-10:number_lines]
     
     for line in last_lines:
-        print(line)
+        print(line, end='')
     
     if (follow):
         while(True):
@@ -24,5 +45,11 @@ def main(path,follow):
                 sys.exit(0)
     else:
         fd.close()
-        
+
+def main():
+    path, follow, lines = parse_args()
+    tail(path, follow, lines)
+
+if __name__ == "__main__":
+    main()
 
